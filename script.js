866ad79b212edf0be7c488ee884dc88348d38e41
Vansh gupta <vanshg6532@gmail.com>
2026-04-03 19:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-//your JS code here. If required.
-const codes = document.querySelectorAll(".code")
+const codes = document.querySelectorAll(".code");
 
-codes[0].focus()
+codes[0].focus();
 
 codes.forEach((code, index)=>{
 
-code.addEventListener("input",(e)=>{
+    code.addEventListener("input",(e)=>{
 
-if(e.target.value.length === 1){
+        if(e.target.value.length === 1){
 
-if(index < codes.length - 1){
-codes[index + 1].focus()
-}
+            if(index < codes.length - 1){
+                codes[index + 1].focus();
+            }
 
-}
+        }
 
-})
+    });
 
-code.addEventListener("keydown",(e)=>{
+    code.addEventListener("keydown",(e)=>{
 
-if(e.key === "Backspace"){
+        if(e.key === "Backspace"){
 
-if(code.value === "" && index > 0){
-codes[index - 1].focus()
-}
+            if(code.value === "" && index > 0){
+                codes[index - 1].focus();
+            } else {
+                code.value = "";
+            }
 
-}
+        }
 
-})
+    });
 
-})
+});
